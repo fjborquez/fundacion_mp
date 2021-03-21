60ca12150cbae3f36f1d4d5498120d64b177f06d
Francisco Bórquez <fborquez@outlook.com>
2021-03-21 17:20:39 -0300

    feat: Obtener json de mercado publico.

--- app/Http/Controllers/MercadoPublicoController.php
+++ app/Http/Controllers/MercadoPublicoController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
 
 class MercadoPublicoController extends Controller
 {
     public function index() {
-        return "HolA MundO";
+        // TODO: Obtener desde configuracion
+        $ticket = "F8537A18-6766-4DEF-9E59-426B4FEE2844";
+
+        // TODO: Obtener desde configuracion 
+        $url = "http://api.mercadopublico.cl/servicios/v1/publico/licitaciones.json";
+
+        // TOOD: Formato fecha desde configuracion
+        $fecha = Carbon::now()->format('dmY');
+
+        // TODO: Controlar codigo y mensaje de respuesta en caso de error
+        $response = Http::get($url, [
+            'fecha' => $fecha,
+            'ticket' => $ticket
+        ]);
+
+        $licitaciones = $response->json();
+
+        return $licitaciones;
     }
 }
