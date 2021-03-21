e2763aa33d0f9c47d017961f1b6444750c163a9b
Francisco Bórquez <fborquez@outlook.com>
2021-03-21 16:45:44 -0300

    commit inicial

---
+++ app/Http/Controllers/MercadoPublicoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MercadoPublicoController extends Controller
+{
+    public function index() {
+        return "HolA MundO";
+    }
+}
