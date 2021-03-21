32f92e799fc1bdab6e11205ac139304bffc93938
Francisco Bórquez <fborquez@outlook.com>
2021-03-21 18:03:27 -0300

    feat: Demo procesar licitaciones.

--- app/Http/Controllers/MercadoPublicoController.php
+++ app/Http/Controllers/MercadoPublicoController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use BenTools\ETL\Etl;
+use BenTools\ETL\EtlBuilder;
+use BenTools\ETL\Transformer\CallableTransformer;
 
 class MercadoPublicoController extends Controller
 {
     public function index() {
+        // TOOD: Si viene de tarea automatica, no enviar a salesforce
+
         // TODO: Obtener desde configuracion
         $ticket = "F8537A18-6766-4DEF-9E59-426B4FEE2844";
 
@@ -19,13 +24,33 @@ class MercadoPublicoController extends Controller
         $fecha = Carbon::now()->format('dmY');
 
         // TODO: Controlar codigo y mensaje de respuesta en caso de error
+        // TODO: Contorlar que la respuesta tenga licitaciones y no sea un mensaje de error
         $response = Http::get($url, [
             'fecha' => $fecha,
             'ticket' => $ticket
         ]);
+        
+        $licitaciones = $response->collect()->get('Listado');
+        $licitacionesProcesadas = [];
+        $etl = EtlBuilder::init()
+            ->transformWith(function($item) {
+                yield array_map('strtolower', $item);
+            })
+            ->loadInto(
+                function ($generated, $key, Etl $etl) use (&$licitacionesProcesadas) {
+                    foreach ($generated as $licitacion) {
+                        // TODO: Filtrar segun palabras clave/excluidas
+                        if (str_contains($licitacion['Nombre'], 'suero')) {
+                            $etl->skipCurrentItem();
+                            break;
+                        }
+                        $licitacionesProcesadas[] = $licitacion;
+                    }
+                })
+            ->createEtl();
 
-        $licitaciones = $response->json();
+        $etl->process($licitaciones);
 
-        return $licitaciones;
+        return $licitacionesProcesadas;
     }
 }
