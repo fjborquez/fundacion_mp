ef5b36279e1f601fbbb43cf6d50d8879997e70b7
Francisco Bórquez <fborquez@outlook.com>
2021-03-23 22:43:15 -0300

    feat: Manejo de errores en http client de laravel.

--- app/Http/Controllers/MercadoPublicoController.php
+++ app/Http/Controllers/MercadoPublicoController.php
@@ -9,47 +9,52 @@ use BenTools\ETL\Etl;
 use BenTools\ETL\EtlBuilder;
 use BenTools\ETL\Transformer\CallableTransformer;
 use App\GeneralSettings;
+use Illuminate\Support\Facades\Log;
 
 class MercadoPublicoController extends Controller
 {
     public function index(GeneralSettings $settings) {
+        $licitacionesProcesadas = [];
         // TOOD: Si viene de tarea automatica, no enviar a salesforce
 
-        // TODO: Obtener desde configuracion
         $ticket = $settings->mercado_publico_ticket;
-
-        // TODO: Obtener desde configuracion 
         $url = $settings->mercado_publico_url_licitaciones;
         $fecha = Carbon::now()->format('dmY');
 
-        // TODO: Controlar codigo y mensaje de respuesta en caso de error
-        // TODO: Contorlar que la respuesta tenga licitaciones y no sea un mensaje de error
         $response = Http::get($url, [
             'fecha' => $fecha,
             'ticket' => $ticket
         ]);
+
+        if ($response->successful()) {
+            if ($response->collect()->has('Listado')) {
+                $licitaciones = $response->collect()->get('Listado');
+                $etl = EtlBuilder::init()
+                    ->transformWith(function($item) {
+                        yield array_map('strtolower', $item);
+                    })
+                    ->loadInto(
+                        function ($generated, $key, Etl $etl) use (&$licitacionesProcesadas) {
+                            foreach ($generated as $licitacion) {
+                                // TODO: Filtrar segun palabras clave/excluidas
+                                if (str_contains($licitacion['Nombre'], 'suero')) {
+                                    $etl->skipCurrentItem();
+                                    break;
+                                }
+                                $licitacionesProcesadas[] = $licitacion;
+                            }
+                        })
+                    ->createEtl();
+
+                $etl->process($licitaciones);
+            } else {
+                Log::error('Error al intentar rescatar listado de licitaciones. No hay campo Listado en el body: ');
+                Log::error($response->body());
+            }
+        } else {
+            Log::error('Error al intentar obtener datos de ' . $url . '?fecha=' . $fecha . '&ticket=' . $ticket);
+        }
         
-        $licitaciones = $response->collect()->get('Listado');
-        $licitacionesProcesadas = [];
-        $etl = EtlBuilder::init()
-            ->transformWith(function($item) {
-                yield array_map('strtolower', $item);
-            })
-            ->loadInto(
-                function ($generated, $key, Etl $etl) use (&$licitacionesProcesadas) {
-                    foreach ($generated as $licitacion) {
-                        // TODO: Filtrar segun palabras clave/excluidas
-                        if (str_contains($licitacion['Nombre'], 'suero')) {
-                            $etl->skipCurrentItem();
-                            break;
-                        }
-                        $licitacionesProcesadas[] = $licitacion;
-                    }
-                })
-            ->createEtl();
-
-        $etl->process($licitaciones);
-
         return $licitacionesProcesadas;
     }
 }
