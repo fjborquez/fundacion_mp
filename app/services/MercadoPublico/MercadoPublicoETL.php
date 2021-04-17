c58f39dc53b0db70cd67986528ac8e16a889ff5c
Francisco Bórquez <fborquez@outlook.com>
2021-04-17 16:12:19 -0400

    fix: separar logica de etl.

--- app/services/MercadoPublico/MercadoPublicoETL.php
+++ app/services/MercadoPublico/MercadoPublicoETL.php
@@ -3,31 +3,24 @@
 namespace App\Services\MercadoPublico;
 
 use App\Services\MercadoPublico\Clients\MercadoPublicoHttpClient;
-use App\Services\MercadoPublico\Clients\BancaEticaSalesforceClient;
-use App\Services\MercadoPublico\Settings\SalesforceSettings;
-use App\Services\MercadoPublico\Settings\EtlSettings;
-
+use App\Services\MercadoPublico\Etl\EtlHelper;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Http;
-use BenTools\ETL\Etl;
-use BenTools\ETL\EtlBuilder;
-use BenTools\ETL\Transformer\CallableTransformer;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\Support\Arr;
-use Omniphx\Forrest\Providers\Laravel\Facades\Forrest;
+use BenTools\ETL\Etl;
+use BenTools\ETL\EtlBuilder;
+use BenTools\ETL\Transformer\CallableTransformer;
 use BenTools\ETL\EventDispatcher\Event\EndProcessEvent;
 use BenTools\ETL\EventDispatcher\Event\ItemExceptionEvent;
 use RuntimeException;
-use Exception;
+use DomainException;
 
 class MercadoPublicoETL {
-    private $salesforceSettings;
-    private $etlSettings;
+    private $etlHelper;
 
     function __construct() {
-        $this->salesforceSettings = app(SalesforceSettings::class);
-        $this->etlSettings = app(EtlSettings::class);
+        $this->etlHelper = new EtlHelper();
     }
 
     public function generarETL($sendToSalesforce = false) {
@@ -36,7 +29,7 @@ class MercadoPublicoETL {
         $fp = fopen(storage_path('framework/locks/etlmp.txt'), "r+");
         
         $this->bloquear($fp);
-        $licitaciones = $this->ejecutar($sendToSalesforce);
+        $licitaciones = $this->ejecutar(boolval($sendToSalesforce));
         $this->desbloquear($fp);
         
         return $licitaciones;
@@ -46,11 +39,11 @@ class MercadoPublicoETL {
         Log::info('Ha iniciado el proceso de ETL');
 
         $licitacionesProcesadas = [];
-        $configuraciones = $this->obtenerConfiguraciones($sendToSalesforce);
         $mercadoPublicoHttpClient = new MercadoPublicoHttpClient();
-        $licitaciones = $mercadoPublicoHttpClient->obtenerLicitacionesConDetalles($configuraciones['fecha']);
-
-        if ($configuraciones['sendToSalesforce']) {
+        $fecha = Carbon::yesterday()->format('dmY');
+        $licitaciones = $mercadoPublicoHttpClient->obtenerLicitacionesConDetalles($fecha);
+        
+        if ($sendToSalesforce) {
             Log::info('Se enviaran licitaciones a Salesforce.');
         }
 
@@ -64,155 +57,34 @@ class MercadoPublicoETL {
                 yield $item;
             })
             ->loadInto(
-                function ($generated, $key, Etl $etl) use (&$licitacionesProcesadas, $configuraciones) {
-                    $listasPalabras = $configuraciones['listasPalabras'];
-                    $bancaEticaSalesforceClient = new BancaEticaSalesforceClient();
-
+                function ($generated, $key, Etl $etl) use (&$licitacionesProcesadas, $sendToSalesforce) {
                     foreach ($generated as $licitacion) {
-                        if (!Arr::exists($licitacion, 'Adjudicacion') || $licitacion['Adjudicacion'] == null) {
+                        if (!$this->etlHelper->isFormatoLicitacionValido($licitacion)) {
+                            throw new DomainException('La licitación no cumple con el formato valido para la ETL.');
+                        }
+
+                        if (!$this->etlHelper->filtrarPorTipoLicitacion($licitacion)) {
                             $etl->skipCurrentItem();
                             break;
                         }
 
-                        if (!Arr::exists($licitacion, 'Items') || count($licitacion['Items']) < 1) {
+                        if (!$this->etlHelper->filtrarPorPalabrasExcluidasNombreLicitacion($licitacion)) {
                             $etl->skipCurrentItem();
                             break;
                         }
 
-                        if (!in_array($licitacion['Tipo'], $listasPalabras['listaTipoLicitacionPermitidos'])) {
+                        if (!$this->etlHelper->categorizarLicitacion($licitacion)) {
                             $etl->skipCurrentItem();
                             break;
                         }
 
-                        if (Str::of($licitacion['Nombre'])->contains($listasPalabras['listaPalabrasExcluidas'])) {
+                        if (!$this->etlHelper->filtrarPorNombreLicitacionExcluidosCategoria($licitacion)) {
                             $etl->skipCurrentItem();
                             break;
                         }
 
-                        if (Str::of($licitacion['Nombre'])->contains($listasPalabras['educacion'])) {
-                            $licitacion['area'] = 'educación y cultura';
-                            $licitacion['sector'] = 'educación';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['industriaCreativa'])) {
-                            $licitacion['area'] = 'educación y cultura';
-                            $licitacion['sector'] = 'industria creativa';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['turismo'])) {
-                            $licitacion['area'] = 'educación y cultura';
-                            $licitacion['sector'] = 'turismo';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['espacioPublico'])) {
-                            $licitacion['area'] = 'desarrollo social';
-                            $licitacion['sector'] = 'espacio público';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['diseño'])) {
-                            $licitacion['area'] = 'desarrollo social';
-                            $licitacion['sector'] = 'diseño';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['vialidad'])) {
-                            $licitacion['area'] = 'desarrollo social';
-                            $licitacion['sector'] = 'vialidad';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['obrasPublicas'])) {
-                            $licitacion['area'] = 'desarrollo social';
-                            $licitacion['sector'] = 'obras públicas';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['salud'])) {
-                            $licitacion['area'] = 'desarrollo social';
-                            $licitacion['sector'] = 'salud';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['inclusion'])) {
-                            $licitacion['area'] = 'desarrollo social';
-                            $licitacion['sector'] = 'inclusión';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['agua'])) {
-                            $licitacion['area'] = 'medio ambiente';
-                            $licitacion['sector'] = 'agua';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['apr'])) {
-                            $licitacion['area'] = 'medio ambiente';
-                            $licitacion['sector'] = 'apr';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['sistemaAlimentarios'])) {
-                            $licitacion['area'] = 'medio ambiente';
-                            $licitacion['sector'] = 'sistemas alimentarios';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['produccionSostenible'])) {
-                            $licitacion['area'] = 'medio ambiente';
-                            $licitacion['sector'] = 'producción sostenible';
-                        } elseif (Str::of($licitacion['Nombre'])->contains($listasPalabras['eficienciaEnergetica'])) {
-                            $licitacion['area'] = 'medio ambiente';
-                            $licitacion['sector'] = 'eficiencia energética';
-                        } else {
-                            $etl->skipCurrentItem();
-                            break;
-                        }
-
-                        if ($licitacion['area'] == 'educación y cultura' 
-                            && Str::of($licitacion['Nombre'])->contains($listasPalabras['excluidasEducacionYCultura'])) {
-                            $etl->skipCurrentItem();
-                            break;
-                        }
-
-                        if ($licitacion['area'] == 'desarrollo social' 
-                            && Str::of($licitacion['Nombre'])->contains($listasPalabras['excluidasDesarrolloSocial'])) {
-                            $etl->skipCurrentItem();
-                            break;
-                        }
-
-                        if ($licitacion['area'] == 'medio ambiente' 
-                            && Str::of($licitacion['Nombre'])->contains($listasPalabras['excluidasMedioAmbiente'])) {
-                            $etl->skipCurrentItem();
-                            break;
-                        }
-
-                        if ($configuraciones['sendToSalesforce']) {
-                            foreach($licitacion['Items']['Listado'] as $item) {
-                                if (Arr::exists($item, 'Adjudicacion') && is_array($item['Adjudicacion'])) {
-                                    $rutProveedor = str_replace('.', '', $item['Adjudicacion']['RutProveedor']);
-                                    $nombreProveedor = $item['Adjudicacion']['NombreProveedor'];
-                
-                                    $accountResponse = $bancaEticaSalesforceClient->obtenerAccountPorRut($rutProveedor);
-                
-                                    $account['id'] = '';
-                                    $lead['id'] = '';
-                
-                                    if ($accountResponse['totalSize'] > 0) {
-                                        $account = [
-                                            'id' => $accountResponse['records'][0]['Id'],
-                                            'rut' => $rutProveedor,
-                                            'nombre' => $nombreProveedor,
-                                            'area' => $licitacion['area'],
-                                            'sector' => $licitacion['sector']
-                                        ];
-                                        $bancaEticaSalesforceClient->actualizarAccount($account);
-                                    } else {
-                                        $leadResponse = $bancaEticaSalesforceClient->obtenerLeadPorRut($rutProveedor);
-                                        $lead = [
-                                            'rut' => $rutProveedor,
-                                            'proveedor' => $nombreProveedor,
-                                            'area' => $licitacion['area'],
-                                            'sector' => $licitacion['sector']
-                                        ];
-
-                                        if ($leadResponse['totalSize'] > 0) {
-                                            $lead['id'] = $leadResponse['records'][0]['Id'];
-                                            $bancaEticaSalesforceClient->actualizarLead($lead);
-                                        } else {
-                                            $lead['nombre'] = $this->salesforceSettings->default_firstname;
-                                            $lead['apellido'] = $this->salesforceSettings->default_lastname;
-                                            $lead['direccion'] = '';
-
-                                            $addLeadResponse = $bancaEticaSalesforceClient->agregarLead($lead);
-                                            $leadId = $addLeadResponse['id'];
-                                        }
-                                    }
-
-                                    $eventoBiografico = [
-                                        'codigo' => $licitacion['CodigoExterno'],
-                                        'nombre' => $licitacion['Nombre'],
-                                        'descripcion' => $licitacion['Descripcion'],
-                                        'tipo' => $licitacion['Tipo'],
-                                        'organismo' => $licitacion['Comprador']['NombreOrganismo'],
-                                        'leadId' => $lead['id'],
-                                        'accountId' => $account['id'],
-                                        'recordTypeId' => $this->salesforceSettings->record_type_id,
-                                        'fecha' => $licitacion['Adjudicacion']['Fecha'],
-                                        'monto' => $item['Adjudicacion']['Cantidad'] * $item['Adjudicacion']['MontoUnitario'],
-                                        'nombreEvento' => $this->salesforceSettings->default_biographical_event_name
-                                    ];
-
-                                    $bancaEticaSalesforceClient->agregarEventoBiografico($eventoBiografico);
-                                } 
-                            }
+                        if ($sendToSalesforce) {
+                            $this->etlHelper->enviarAdjudicacionesASalesforce($licitacion);
                         }
 
                         $licitacionesProcesadas[] = $licitacion;
@@ -232,56 +104,7 @@ class MercadoPublicoETL {
         return $licitacionesProcesadas;
     }
 
-    function obtenerConfiguraciones($sendToSalesforce) {
-        $configuraciones = [];
-        $configuraciones['sendToSalesforce'] = boolval($sendToSalesforce);
-        $configuraciones['fecha'] = Carbon::yesterday()->format('dmY');
-
-        $listaTipoLicitacionPermitidos = explode(';', $this->etlSettings->filtro_tipo_licitacion);
-        $listaPalabrasExcluidas = explode(';', $this->etlSettings->filtro_palabras_excluidas);
-        $listaPalabrasClaveEducacion = explode(';', $this->etlSettings->filtro_palabras_clave_educacion);
-        $listaPalabrasClaveIndustriaCreativa = explode(';', $this->etlSettings->filtro_palabras_clave_industria_creativa);
-        $listaPalabrasClaveTurismo = explode(';', $this->etlSettings->filtro_palabras_clave_turismo);
-        $listaPalabrasClaveEspacioPublico = explode(';', $this->etlSettings->filtro_palabras_clave_espacio_publico);
-        $listaPalabrasClaveDiseño = explode(';', $this->etlSettings->filtro_palabras_clave_diseño);
-        $listaPalabrasClaveVialidad = explode(';', $this->etlSettings->filtro_palabras_clave_vialidad);
-        $listaPalabrasClaveObrasPublicas = explode(';', $this->etlSettings->filtro_palabras_clave_obras_publicas);
-        $listaPalabrasClaveSalud = explode(';', $this->etlSettings->filtro_palabras_clave_salud);
-        $listaPalabrasClaveInclusion = explode(';', $this->etlSettings->filtro_palabras_clave_inclusion);
-        $listaPalabrasClaveAgua = explode(';', $this->etlSettings->filtro_palabras_clave_agua);
-        $listaPalabrasClaveApr = explode(';', $this->etlSettings->filtro_palabras_clave_apr);
-        $listaPalabrasClaveSistemasAlimentarios = explode(';', $this->etlSettings->filtro_palabras_clave_sistemas_alimentarios);
-        $listaPalabrasClaveProduccionSostenible = explode(';', $this->etlSettings->filtro_palabras_clave_produccion_sostenible);
-        $listaPalabrasClaveEficienciaEnergetica = explode(';', $this->etlSettings->filtro_palabras_clave_eficiencia_energetica);
-        $listaPalabrasExcluidasEducacionYCultura = explode(';', $this->etlSettings->filtro_palabras_excluidas_educacion_y_cultura);
-        $listaPalabrasExcluidasDesarrolloSocial = explode(';', $this->etlSettings->filtro_palabras_excluidas_desarrollo_social);
-        $listaPalabrasExcluidasMedioAmbiente = explode(';', $this->etlSettings->filtro_palabras_excluidas_medio_ambiente);
-
-        $configuraciones['listasPalabras'] = [
-            'listaTipoLicitacionPermitidos' => $listaTipoLicitacionPermitidos,
-            'listaPalabrasExcluidas' => $listaPalabrasExcluidas,
-            'educacion' => $listaPalabrasClaveEducacion,
-            'industriaCreativa' => $listaPalabrasClaveIndustriaCreativa,
-            'turismo' => $listaPalabrasClaveTurismo,
-            'espacioPublico' => $listaPalabrasClaveEspacioPublico,
-            'diseño' => $listaPalabrasClaveDiseño,
-            'vialidad' => $listaPalabrasClaveVialidad,
-            'obrasPublicas' => $listaPalabrasClaveObrasPublicas,
-            'salud' => $listaPalabrasClaveSalud,
-            'inclusion' => $listaPalabrasClaveInclusion,
-            'agua' => $listaPalabrasClaveAgua,
-            'apr' => $listaPalabrasClaveApr,
-            'sistemaAlimentarios' => $listaPalabrasClaveSistemasAlimentarios,
-            'produccionSostenible' => $listaPalabrasClaveProduccionSostenible,
-            'eficienciaEnergetica' => $listaPalabrasClaveEficienciaEnergetica,
-            'excluidasEducacionYCultura' => $listaPalabrasExcluidasEducacionYCultura,
-            'excluidasDesarrolloSocial' => $listaPalabrasExcluidasDesarrolloSocial,
-            'excluidasMedioAmbiente' => $listaPalabrasExcluidasMedioAmbiente
-        ];
-
-        return $configuraciones;
-    }
-
+    // FUNCIONES DE MUTEX
     private function bloquear($fp) {
         if (!flock($fp, LOCK_EX | LOCK_NB)) {
             fclose($fp);
