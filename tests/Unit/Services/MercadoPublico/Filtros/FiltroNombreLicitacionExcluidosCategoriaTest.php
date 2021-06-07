096d2665f1443b49d64011eb030edef898d43826
Francisco Bórquez <fborquez@outlook.com>
2021-06-06 20:59:51 -0400

    feat: incluir descripcion en filtro de excluidos por categoria.

--- tests/Unit/Services/MercadoPublico/Filtros/FiltroNombreLicitacionExcluidosCategoriaTest.php
+++ tests/Unit/Services/MercadoPublico/Filtros/FiltroNombreLicitacionExcluidosCategoriaTest.php
@@ -10,12 +10,16 @@ class FiltroNombreLicitacionExcluidosCategoriaTest extends TestCase
 {
     protected $licitacionConNombreExcluido;
     protected $licitacionSinNombreExcluido;
+    protected $licitacionConDescripcionExcluido;
+    protected $licitacionSinDescripcionExcluido;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->setUpLicitacionConNombreExcluido();
         $this->setUpLicitacionSinNombreExcluido();
+        $this->setUpLicitacionConDescripcionExcluido();
+        $this->setUpLicitacionSinDescripcionExcluido();
     }
 
     private function setUpLicitacionConNombreExcluido()
@@ -30,6 +34,18 @@ class FiltroNombreLicitacionExcluidosCategoriaTest extends TestCase
         $this->openLicitacion($path, $this->licitacionSinNombreExcluido);
     }
 
+    private function setUpLicitacionConDescripcionExcluido()
+    {
+        $path = storage_path("testing/json/licitacion_con_palabras_excluidas_descripcion_area_sector.json");
+        $this->openLicitacion($path, $this->licitacionConDescripcionExcluido);
+    }
+
+    private function setUpLicitacionSinDescripcionExcluido()
+    {
+        $path = storage_path("testing/json/licitacion_descripcion_no_excluida_area_sector.json");
+        $this->openLicitacion($path, $this->licitacionSinDescripcionExcluido);
+    }
+
     private function openLicitacion($path, &$licitacion)
     {
         $json = file_get_contents($path);
@@ -37,7 +53,7 @@ class FiltroNombreLicitacionExcluidosCategoriaTest extends TestCase
         $licitacion = $licitacionJson['Listado'][0];
     }
 
-     /**
+    /**
      * A basic unit test example.
      *
      * @return void
@@ -74,6 +90,46 @@ class FiltroNombreLicitacionExcluidosCategoriaTest extends TestCase
         $modificadorReturn = $modificador->ejecutar($this->licitacionSinNombreExcluido);
 
         $this->assertEquals($this->licitacionSinNombreExcluido['Nombre'], strtoupper($this->licitacionSinNombreExcluido['Nombre']));
+        $this->assertTrue($modificadorReturn);
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_Should_ReturnTrue_When_DescriptionLicitacionIsNotExcludedAndLower()
+    {
+        $modificador = new FiltroNombreLicitacionExcluidosCategoria();
+        $this->licitacionSinDescripcionExcluido['Descripcion'] = strtolower($this->licitacionSinDescripcionExcluido['Descripcion']);
+        $modificadorReturn = $modificador->ejecutar($this->licitacionSinDescripcionExcluido);
+        
+        $this->assertEquals($this->licitacionSinDescripcionExcluido['Descripcion'], strtolower($this->licitacionSinDescripcionExcluido['Descripcion']));
+        $this->assertTrue($modificadorReturn);
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_Should_ReturnFalse_When_DescriptionLicitacionIsExcludedAndLower()
+    {
+        $modificador = new FiltroNombreLicitacionExcluidosCategoria();
+        $this->licitacionConDescripcionExcluido['Descripcion'] = strtolower($this->licitacionConDescripcionExcluido['Descripcion']);
+        $modificadorReturn = $modificador->ejecutar($this->licitacionConDescripcionExcluido);
+
+        $this->assertEquals($this->licitacionConDescripcionExcluido['Descripcion'], strtolower($this->licitacionConDescripcionExcluido['Descripcion']));
+        $this->assertFalse($modificadorReturn);
+    }
+
+    public function test_Should_ReturnTrue_When_DescriptionLicitacionIsExcludedAndUpper()
+    {
+        $modificador = new FiltroNombreLicitacionExcluidosCategoria();
+        $this->licitacionSinDescripcionExcluido['Descripcion'] = strtoupper($this->licitacionSinDescripcionExcluido['Nombre']);
+        $modificadorReturn = $modificador->ejecutar($this->licitacionSinDescripcionExcluido);
+
+        $this->assertEquals($this->licitacionSinDescripcionExcluido['Nombre'], strtoupper($this->licitacionSinDescripcionExcluido['Nombre']));
         $this->assertTrue($modificadorReturn);
     }
 }
